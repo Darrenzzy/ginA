ca8910fc277c7938710c8b1010a065c4a3b5b3d8
darren911 <zzyphp@gmail.com>
2022-09-21 14:28:20 +0800

    feat : finish

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 
 	pool "github.com/bitleak/go-redis-pool/v2"
@@ -20,6 +22,7 @@ var (
 	dbGroup  *xorm.EngineGroup
 	pools    *pool.Pool
 	mqClient *redis.Client
+	mutex    sync.Mutex
 )
 
 func init() {
@@ -28,26 +31,20 @@ func init() {
 	initMq()
 }
 
-func connStr(db string) string {
-	conn := "test:test@"
-	conn += "tcp(127.0.0.1:3306)"
-	// %s:%s@tcp(%s:3306)/%s?charset=utf8
-	return conn + "/" + db + "?charset=utf8"
-}
-
 func initMysql() {
-	engine, err := xorm.NewEngine("mysql", connStr("test"))
+	engine, err := xorm.NewEngine("mysql", fmt.Sprintf("test:test@tcp(127.0.0.1:3306)/%s?charset=utf8", "test"))
 	if err != nil {
 		log.Fatalln(err)
 		return
 	}
-	// defer engine.Close()
 	engine.ShowSQL(true)
 	dbGroup, err = xorm.NewEngineGroup(engine, []*xorm.Engine{engine, engine})
 	if err != nil {
 		log.Fatalln(err)
 		return
 	}
+	log.Println("mysql 启动完毕")
+
 }
 
 func initRedis() {
@@ -71,6 +68,7 @@ func initRedis() {
 	if err != nil {
 		log.Fatal(err, 222)
 	}
+	log.Println("redis 启动完毕")
 
 }
 
@@ -81,11 +79,16 @@ func initMq() {
 		DB:       0,           // use default DB
 	})
 	pong, err := mqClient.Ping().Result()
-	fmt.Println(pong, err)
+	if err == nil && pong != "" {
+		log.Println("mq 启动完毕")
+	}
 }
 
 func main() {
-	r := gin.Default()
+
+	r := gin.New()
+	r.Use(gin.Logger())
+	gin.SetMode("release")
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -99,14 +102,17 @@ func main() {
 	r.POST("/del", DelArticle)
 
 	go topicHandle()
+
+	var port = flag.Int64("port", 8100, "端口")
+	flag.Parse()
 	server := &http.Server{
-		Addr:    ":8100",
+		Addr:    fmt.Sprintf(":%d", *port),
 		Handler: r,
 	}
 	// 监听并启动服务
 	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
 }
@@ -115,7 +121,7 @@ func GetArticle(c *gin.Context) {
 	id, _ := c.GetQuery("id")
 	idInt, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	if idInt != 0 {
 		data := getArticle(idInt)
@@ -134,18 +140,18 @@ func GetArticle(c *gin.Context) {
 }
 
 func DelArticle(c *gin.Context) {
-
-	ctx := context.Background()
-
-	s := pools.Set(ctx, "name", "barry", time.Second*60).String()
-	fmt.Println(s)
-
-	data := &Article{}
-	row, err := dbGroup.Where("id=2").Delete(data)
-	log.Println(row, err)
+	data := Article{}
+	err := c.BindJSON(&data)
+	if err != nil {
+		log.Println(err)
+	}
+	if data.Id > 0 {
+		row, err := dbGroup.Where("id=?", data.Id).Delete(&data)
+		log.Println(row, err)
+	}
 
 	c.JSON(200, gin.H{
-		"message": "pong",
+		"message": "ok",
 	})
 }
 
@@ -154,46 +160,41 @@ func EditArticle(c *gin.Context) {
 	data := Article{}
 	err := c.BindJSON(&data)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
 	oldData := getArticle(data.Id)
 	var needUpdata bool
-	if data.Content != "" {
+	if data.Content != oldData.Content {
 		oldData.Content = data.Content
 		needUpdata = true
 	}
 
-	if data.Email != "" {
+	if data.Email != oldData.Email {
 		oldData.Email = data.Email
 		needUpdata = true
-
 	}
 
-	if data.Author != "" {
+	if data.Author != oldData.Author {
 		oldData.Author = data.Author
 		needUpdata = true
 
 	}
 	message := "ok"
 	if needUpdata {
-		err := editArticle(oldData)
-		if err != nil {
-			message = err.Error()
-		}
+		bs, _ := json.Marshal(data)
+		mqClient.Publish("topic", string(bs))
 	}
-	// data:=getArticle(data.Id)
 	c.JSON(200, gin.H{
 		"message": message,
 	})
 }
 
 func CreateArticle(c *gin.Context) {
-
 	data := Article{}
 	err := c.BindJSON(&data)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	createArticle(&data)
 	c.JSON(200, gin.H{
@@ -204,8 +205,11 @@ func CreateArticle(c *gin.Context) {
 func FindArticles(c *gin.Context) {
 	id, _ := c.GetQuery("id")
 	lastId, err := strconv.ParseInt(id, 10, 64)
-	list := []*Article{}
-	err = dbGroup.Slave().Where("id > ?", lastId).Limit(2).Find(&list)
+	if err != nil {
+		log.Println(err)
+	}
+	list := make([]*Article, 0, 10)
+	err = dbGroup.Slave().Where("id > ?", lastId).Limit(10).Find(&list)
 	if err != nil {
 		log.Println(err)
 	}
@@ -224,7 +228,7 @@ type Article struct {
 	Email     string    `xorm:"email" sql:"email" json:"email"`
 	Author    string    `xorm:"author" sql:"author" json:"author"`
 	Updated   time.Time `xorm:"update_by" json:"updated"`
-	DeletedAt time.Time `xorm:"deleted"`
+	DeletedAt time.Time `xorm:"deleted" json:"_"`
 }
 
 func (n *Article) TableName() string {
@@ -241,14 +245,14 @@ func getArticle(id int64) (data *Article) {
 	if art != "" {
 		err := json.Unmarshal([]byte(art), data)
 		if err != nil {
-			fmt.Println(err, 22)
+			log.Println(err, 22)
 		}
 		return data
 	}
 
 	_, err := dbGroup.Slave().Where("id=?", id).Get(data)
 	if err != nil {
-		fmt.Println(err, 11)
+		log.Println(err, 11)
 	}
 	bs, _ := json.Marshal(data)
 	pools.Set(ctx, key, string(bs), time.Hour*24*3).Val()
@@ -265,7 +269,7 @@ func editArticle(data *Article) error {
 		Author:  data.Author,
 	})
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	bs, _ := json.Marshal(data)
 	pools.Set(ctx, key, string(bs), time.Hour*24*3).Val()
@@ -278,32 +282,71 @@ func createArticle(data *Article) {
 	mqClient.Publish("topic", string(bs))
 }
 
-func createArticleToDb(art string) {
+func articleToDb(art string) {
 	data := &Article{}
 	err := json.Unmarshal([]byte(art), data)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	row, err := dbGroup.Master().InsertOne(data)
-	if err != nil {
-		fmt.Println(err)
+	isLock := lock(fmt.Sprintf("art_%d", data.Id))
+	defer unLock(fmt.Sprintf("art_%d", data.Id))
+	if isLock {
+		if data.Id > 0 {
+			err := editArticle(data)
+			if err != nil {
+				log.Println("失败更新db  id", err)
+				return
+			}
+			log.Printf("成功更新db  id：%d", data.Id)
+			return
+		}
+		row, err := dbGroup.Master().InsertOne(data)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Printf("成功写入db：%d条", row)
 	}
-	log.Printf("成功写入db：%d条", row)
+
 }
 
 func topicHandle() {
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Println("topicHandle Recovered in f", r)
+			log.Println("topicHandle Recovered in f", r)
 		}
 	}()
-	println(4444)
 
-	pubsub := mqClient.PSubscribe("topic")
-	defer pubsub.Close()
-	for msg := range pubsub.Channel() {
-		fmt.Printf("channel=%s message=%s\n", msg.Channel, msg.Payload)
-		createArticleToDb(msg.Payload)
+	key := fmt.Sprintf("liten_%d_%d", time.Now().Day(), time.Now().Minute()/10)
+	ident, _ := pools.Get(context.TODO(), key).Int()
+	pools.SetNX(context.TODO(), key, 1, time.Minute)
+	if ident == 0 {
+		println("启动消费mq成功")
+		pubsub := mqClient.PSubscribe("topic")
+		defer pubsub.Close()
+		for msg := range pubsub.Channel() {
+			log.Printf("channel=%s message=%s\n", msg.Channel, msg.Payload)
+			articleToDb(msg.Payload)
+		}
 	}
 
+}
+
+func lock(key string) bool {
+	var ctx = context.Background()
+	mutex.Lock()
+	defer mutex.Unlock()
+	bools, err := pools.SetNX(ctx, key, 1, 10*time.Second).Result()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return bools
+}
+func unLock(key string) int64 {
+	var ctx = context.Background()
+	nums, err := pools.Del(ctx, key)
+	if err != nil {
+		log.Println(err.Error())
+		return 0
+	}
+	return nums
 }
--- main_test.go
+++ main_test.go
@@ -25,3 +25,45 @@ func Test_getArticle(t *testing.T) {
 		})
 	}
 }
+
+func Test_editArticle(t *testing.T) {
+	type args struct {
+		data *Article
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "edit", args: args{&Article{
+			Id:      10,
+			Content: "Content",
+			Author:  "xxx",
+			Email:   "xxx@qq.com",
+		}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := editArticle(tt.args.data); (err != nil) != tt.wantErr {
+				t.Errorf("editArticle() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_articleToDb(t *testing.T) {
+	type args struct {
+		art string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "write", args: args{art: ""}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			articleToDb(tt.args.art)
+		})
+	}
+}
